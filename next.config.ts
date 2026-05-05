0a6433048b0418978ab8d0a6bccdf4f5330e98f9
dimssu <aryansi1126@gmail.com>
2026-05-05 13:10:22 +0530

    chore: scaffold next.js app

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingRoot: __dirname,
+  devIndicators: false,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "api.dicebear.com" },
+    ],
+  },
 };
 
 export default nextConfig;
